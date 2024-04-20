38d31220de9dbc2102ec98caa13d71b38c3fde02
kponire <kponire@gmail.com>
2024-04-20 04:37:32 -0700

    flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 """
-Script that runs a flask app
-
+start Flask application
 """
+
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """
-    Function that returns on url
-    """
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """
-    Function that returns on url /hbnb
-
-    """
+    """returns HBNB"""
     return 'HBNB'
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
